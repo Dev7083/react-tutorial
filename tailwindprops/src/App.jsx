e2e2cb0d282ae38e48a7fac51196ccaa1aea1259
dev7083 <113914000+Dev7083@users.noreply.github.com>
2024-08-05 17:32:24 +0530

    added props

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -6,12 +6,12 @@ function App() {
   return (
     <>
       <main className="">
-        <h1 className=' text-black p-4 rounded-xl mb-4 font-Montserrat'>Tailwind Test</h1>
+        <h1 className=' text-black p-4 rounded-xl mb-4 font-Montserrat'>React Props</h1>
         <section className="xl:padding-l wide:padding-r padding-b flex gap-2 ">
 
-          <Cards />
-          <Cards />
-          <Cards />
+          <Cards productName="Macbook Air" content="Entry level Laptop" />
+          <Cards productName="Macbook Air M2" content="LAptop with M2 Chip" />
+          <Cards productName="Macbook Pro" content="Laptop for professionals" />
         </section>
       </main>
     </>
--- tailwindprops/src/components/Cards.jsx
+++ tailwindprops/src/components/Cards.jsx
@@ -1,5 +1,5 @@
 import { ArrowUpRight } from 'lucide-react';
-export const Cards = () => {
+export const Cards = ({ productName, content }) => {
     return (
         <>
             <div className="w-[300px] rounded-md border">
@@ -10,11 +10,11 @@ export const Cards = () => {
                 />
                 <div className="p-4">
                     <h1 className="inline-flex items-center text-lg font-semibold">
-                        About Macbook &nbsp;
+                        {productName} &nbsp;
                         <ArrowUpRight className="h-4 w-4" />
                     </h1>
                     <p className="mt-3 text-sm text-gray-600">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?
+                        {content}
                     </p>
                     <div className="mt-4">
                         <span className="mb-2 mr-2 inline-block rounded-full bg-gray-100 px-3 py-1 text-[10px] font-semibold text-gray-900">
@@ -31,7 +31,7 @@ export const Cards = () => {
                         type="button"
                         className="mt-4 w-full rounded-sm bg-black px-2 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
                     >
-                        Read
+                        Read More
                     </button>
                 </div>
             </div>
