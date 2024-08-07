f7fb162eeafc33d25cedcff84799faabbb3f0255
dev7083 <113914000+Dev7083@users.noreply.github.com>
2024-08-07 22:10:53 +0530

    alpha

--- 05_passwordGen/src/App.jsx
+++ 05_passwordGen/src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [length, setlength] = useState(8)
+  const [numallowed, setnumallowed] = useState(8)
   return (
     <>
-      <div className=""></div>
+      <h1 className="text-4xl text-center text-white"> Password Generator</h1>
     </>
+
   )
 }
 
