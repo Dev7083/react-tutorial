f2f9f8e0e22543abdbf66dfe6ac1b7908d9cba58
dev7083 <113914000+Dev7083@users.noreply.github.com>
2024-08-17 21:30:56 +0530

    React Router

---
+++ 07_react_router/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import { Home, Header, About, Contact, Footer } from './components'
+function App() {
+
+
+  return (
+    <>
+      <Header />
+      <Home />
+      <About />
+      <Contact />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ 07_react_router/src/components/index.js
@@ -0,0 +1,7 @@
+import Home from "./Home";
+import Header from "./Header";
+import About from "./About";
+import Contact from "./Contact";
+import Footer from "./Footer";
+
+export { Home, Header, About, Contact, Footer }
