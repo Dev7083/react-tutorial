caf10ac6213e6861070e5729ce10562058d2facc
dev7083 <113914000+Dev7083@users.noreply.github.com>
2024-08-18 21:13:57 +0530

    context API finished

--- 09_ThemeSwitcher_ContextAPI/src/App.jsx
+++ 09_ThemeSwitcher_ContextAPI/src/App.jsx
@@ -1,17 +1,43 @@
 
+import { useState, useEffect } from 'react'
 import './App.css'
-import Login from './components/login'
-import Profile from './components/profile'
-import UserContextProvider from './context/UserContextProvider'
+import { ThemeProvider } from './context/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+  // actual change in theme
+  useEffect(() => {
+    document.querySelector('html').classList.remove("light", "dark")
+    document.querySelector('html').classList.add(themeMode)
+  }, [themeMode])
+
 
   return (
-    <UserContextProvider>
-      <h1>React With Chai & Share is Important</h1>
-      <Login/>
-      <Profile/>
-    </UserContextProvider>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* Theme Button */}
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* Card */}
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+
   )
 }
 
