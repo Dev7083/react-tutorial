5e12561c48252e8d483b891e827fc7ae9b9acfc9
dev7083 <113914000+Dev7083@users.noreply.github.com>
2024-08-18 10:58:01 +0530

    beta

--- 07_react_router/src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Home from "./Home";
-import Header from "./Header";
-import About from "./About";
-import Contact from "./Contact";
-import Footer from "./Footer";
-
-export { Home, Header, About, Contact, Footer }
