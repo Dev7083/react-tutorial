6adaa1313b4450eef429c071ba0203df1c91cea6
dev7083 <113914000+Dev7083@users.noreply.github.com>
2024-09-15 22:20:36 +0530

    added components

--- MajorProject_Blog/src/components/index.js
+++ MajorProject_Blog/src/components/index.js
@@ -1,7 +1,12 @@
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
-
+import LogoutBtn from "./Header/LogoutBtn";
+import Container from './container/Container';
+import Logo from "./Logo";
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn
 }
